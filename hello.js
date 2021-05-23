5a95d69de09f20cec367b6df07246abf6d480a13
anandjos <anandjose5@gmail.com>
2021-05-23 17:25:48 +0530

    Add fahrenheit to celsius converter

--- hello.js
+++ hello.js
@@ -2,3 +2,14 @@ function printName(){
   var name = document.getElementById('name').value;
   document.getElementById('display').innerHTML = "Hello " + name;
 }
+function converttoCelsius(){
+  var fahrenheit = document.getElementById('fahrenheit').value;
+  if(isNaN(fahrenheit))
+  {
+    alert('Enter valid value');
+    return;
+  }
+  var celsius = (fahrenheit - 32)*5/9;
+  celsius = celsius.toFixed(2);
+  document.getElementById('celsius').value = celsius;
+}
